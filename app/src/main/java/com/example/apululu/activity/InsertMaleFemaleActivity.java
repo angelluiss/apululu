f493d2cbce98bd31af2a432499256a8629a91f60
Angel Rangel <oslomrp@gmail.com>
2019-06-26 16:26:36 -0400

    Transiciones_Parse_BD_Parametros

--- app/src/main/java/com/example/apululu/activity/InsertMaleFemaleActivity.java
+++ app/src/main/java/com/example/apululu/activity/InsertMaleFemaleActivity.java
@@ -6,22 +6,73 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.apululu.R;
 
 public class InsertMaleFemaleActivity extends AppCompatActivity {
 
+    // Variables para clickeo
+    int clickedMale = 0;
+    int clickedFemale = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_male_female);
-        LinearLayout buttonNext = (LinearLayout) findViewById(R.id.llButtonMaleFemale);
+
+
+        // Instaciaciòn de los Widgets
+        final LinearLayout buttonNext = (LinearLayout) findViewById(R.id.llButtonMaleFemale);
+        final LinearLayout buttonMale = (LinearLayout) findViewById(R.id.llButtonMale);
+        final TextView male = (TextView) findViewById(R.id.tvMale);
+        final LinearLayout buttonFemale = (LinearLayout) findViewById(R.id.llButtonFemale);
+        final TextView female = (TextView) findViewById(R.id.tvFemale);
+
+
+
+        //-------------- Click Male Button Cambia de color
+        buttonMale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((clickedMale != clickedFemale)){
+                    clickedMale++;
+                    buttonFemale.setBackgroundResource(R.drawable.button_rounded_gray);
+                    female.setTextColor(getResources().getColor(R.color.color_gray));
+                    buttonMale.setBackgroundResource(R.drawable.button_rounded_blue);
+                    male.setTextColor(getResources().getColor(R.color.black));
+                }
+            }
+        });
+        //-------------- Click Female Button Cambia de color
+        buttonFemale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((clickedFemale == clickedMale) || clickedFemale >= 1){
+                    clickedFemale++;
+                    buttonMale.setBackgroundResource(R.drawable.button_rounded_gray);
+                    male.setTextColor(getResources().getColor(R.color.color_gray));
+                    buttonFemale.setBackgroundResource(R.drawable.button_rounded_blue);
+                    female.setTextColor(getResources().getColor(R.color.black));
+                }
+            }
+        });
+
+
+        //-------- Boton Next Verificación de estado de los botones
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //button.setBackgroundResource(R.drawable.button_rounded_blue);
-                Intent intent = new Intent(InsertMaleFemaleActivity.this, InsertCalendarActivity.class);
-                startActivity(intent);
+
+                if (clickedMale > 0 || clickedFemale > 1){
+                    buttonNext.setBackgroundResource(R.drawable.rounded_button_gradient_solid);
+                    Intent intent = new Intent(InsertMaleFemaleActivity.this, InsertCalendarActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(InsertMaleFemaleActivity.this, "Select a gender", Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
--- app/src/main/java/com/example/apululu/activity/InsertNameActivity.java
+++ app/src/main/java/com/example/apululu/activity/InsertNameActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.apululu.R;
 
@@ -16,14 +18,25 @@ public class InsertNameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_insert_name);
 
         final LinearLayout buttonNext = (LinearLayout) findViewById(R.id.llButtonName);
+        final EditText firtsName = (EditText) findViewById(R.id.etName);
+        final EditText lastName = (EditText) findViewById(R.id.etLastName);
 
 
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String firtsNameV = firtsName.getText().toString();
+                String lastNameV = lastName.getText().toString();
+                if(!firtsNameV.isEmpty() && !lastNameV.isEmpty()){
+                    buttonNext.setBackgroundResource(R.drawable.rounded_button_gradient_solid);
+                    Intent intent4 = new Intent(InsertNameActivity.this,InsertMaleFemaleActivity.class);
+                    startActivity(intent4);
+                }else if(firtsNameV.isEmpty()){
+                    Toast.makeText(InsertNameActivity.this,"Insert your First Name", Toast.LENGTH_LONG).show();
+                }else if(lastNameV.isEmpty()){
+                    Toast.makeText(InsertNameActivity.this,"Insert your Last Name", Toast.LENGTH_LONG).show();
+                }
 
-                Intent intent4 = new Intent(InsertNameActivity.this,InsertMaleFemaleActivity.class);
-                startActivity(intent4);
             }
         });
     }
