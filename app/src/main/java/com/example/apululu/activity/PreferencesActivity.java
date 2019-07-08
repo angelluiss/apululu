43eaba5629e9eac7aeb0070805ecd6206f294847
Angel Rangel <oslomrp@gmail.com>
2019-07-08 14:05:58 -0400

    Ajuste_widgets_preference_BD

--- app/src/main/java/com/example/apululu/activity/PreferencesActivity.java
+++ app/src/main/java/com/example/apululu/activity/PreferencesActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.apululu.R;
 
@@ -22,9 +26,53 @@ public class PreferencesActivity extends AppCompatActivity {
         LinearLayout buttonFeed = (LinearLayout) findViewById(R.id.llFeedButton);
         SwitchCompat scPlay = (SwitchCompat) findViewById(R.id.scPlayGame);
 
+        //** Instancia del Seek bar Age
+        final SeekBar seekBarAge = (SeekBar) findViewById(R.id.sbAgePreference);
+        final TextView seekbarText = (TextView) findViewById(R.id.tvSBAge);
+
+        //** Instancia del Seek bar Distancia
+        final SeekBar seekBarDistance = (SeekBar)findViewById(R.id.sbDistancePreference);
+        final TextView seekbarTextDistance = (TextView) findViewById(R.id.tvSBDistance);
+
+        //*** Mostrar el valor de la barra de edad
+        seekBarAge.setOnSeekBarChangeListener (new OnSeekBarChangeListener() {
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                final int seekBarAgeValue = seekBarAge.getProgress();
+                final String sBAgeVAlue = Integer.toString(seekBarAgeValue);
+                seekbarText.setText(sBAgeVAlue);
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        //*** Mostrar el valor de la barra de edad
+        seekBarDistance.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                final int seekBarDistanceValue = seekBarDistance.getProgress();
+                final String sBdistanceValue = Integer.toString(seekBarDistanceValue);
+                seekbarText.setText(sBdistanceValue);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
-        // *** Click listener para pasar de actividades 
+        // *** Click listener para Menu
         buttonProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
