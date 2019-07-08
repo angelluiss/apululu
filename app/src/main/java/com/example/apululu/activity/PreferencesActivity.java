6cd284f86feda095b05ac7026eea61314a4ce039
Angel Rangel <oslomrp@gmail.com>
2019-07-08 15:56:55 -0400

    obtencion_login_token

--- app/src/main/java/com/example/apululu/activity/PreferencesActivity.java
+++ app/src/main/java/com/example/apululu/activity/PreferencesActivity.java
@@ -14,11 +14,20 @@ import com.example.apululu.R;
 
 public class PreferencesActivity extends AppCompatActivity {
 
+    int clickedMalePreference = 0;
+    int clickedFemalePreference = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
 
+        //** Botones de seleccion
+        final LinearLayout buttonMale = (LinearLayout) findViewById(R.id.llGenderPreferenceMale);
+        final LinearLayout buttonFemale = (LinearLayout) findViewById(R.id.llGenderPreferenceFemale);
+        LinearLayout buttonWork = (LinearLayout) findViewById(R.id.llWorkPreference);
+        LinearLayout buttonStudent = (LinearLayout) findViewById(R.id.llStudyPreference);
+        LinearLayout buttonOther = (LinearLayout) findViewById(R.id.llOtherPreference);
 
         //*** Instancia de los Widgets del Menu
         LinearLayout buttonProfile = (LinearLayout) findViewById(R.id.llProfileButton);
@@ -32,11 +41,14 @@ public class PreferencesActivity extends AppCompatActivity {
         //** Instancia del Seek bar Distancia
         final SeekBar seekBarDistance = (SeekBar)findViewById(R.id.sbDistancePreference);
         final TextView seekbarTextDistance = (TextView) findViewById(R.id.tvSBDistance);
-        
+
+
+
+        //*** Mostrar el valor de la barra de edad //--------------------------------------------------------------------------------------------
         seekBarAge.setProgress(18);
         seekbarTextDistance.setText("200");
         seekBarDistance.setProgress(200);
-        //*** Mostrar el valor de la barra de edad
+
         seekBarAge.setOnSeekBarChangeListener (new OnSeekBarChangeListener() {
 
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -51,9 +63,9 @@ public class PreferencesActivity extends AppCompatActivity {
 
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
-        });
+        }); //--------------------------------------------------------------------------------------------
 
-        //*** Mostrar el valor de la barra de edad
+        //*** Mostrar el valor de la barra de edad //--------------------------------------------------------------------------------------------
         seekBarDistance.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -71,10 +83,10 @@ public class PreferencesActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
-        });
+        });  //--------------------------------------------------------------------------------------------
 
 
-        // *** Click listener para Menu
+        // *** Click listener para Menu //--------------------------------------------------------------------------------------------
         buttonProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -97,6 +109,31 @@ public class PreferencesActivity extends AppCompatActivity {
                 Intent intent = new Intent(PreferencesActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
+        });//--------------------------------------------------------------------------------------------
+
+        //*** Configuracion  de los efectos de los Botones de Preferencias
+        buttonMale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((clickedMalePreference != clickedFemalePreference)){
+                    clickedMalePreference++;
+                    buttonFemale.setBackgroundResource(R.drawable.button_rounded_dark_gray);
+                    buttonMale.setBackgroundResource(R.drawable.button_rounded_blue);
+                }
+            }
         });
+
+        buttonFemale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((clickedFemalePreference == clickedMalePreference) || clickedFemalePreference >= 1){
+                    clickedFemalePreference++;
+                    buttonMale.setBackgroundResource(R.drawable.button_rounded_dark_gray);
+                    buttonFemale.setBackgroundResource(R.drawable.button_rounded_blue);
+                }
+            }
+        });
+
+
     }
 }
