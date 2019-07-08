5295365d5b312629e70a92b1673381949994670b
Angel Rangel <oslomrp@gmail.com>
2019-07-08 13:48:02 -0400

    Preference_widgets_configuration

--- app/src/main/java/com/example/apululu/activity/PreferencesActivity.java
+++ app/src/main/java/com/example/apululu/activity/PreferencesActivity.java
@@ -1,7 +1,11 @@
 package com.example.apululu.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.apululu.R;
 
@@ -13,5 +17,36 @@ public class PreferencesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_preferences);
 
 
+        //*** Instancia de los Widgets del Menu
+        LinearLayout buttonProfile = (LinearLayout) findViewById(R.id.llProfileButton);
+        LinearLayout buttonFeed = (LinearLayout) findViewById(R.id.llFeedButton);
+        SwitchCompat scPlay = (SwitchCompat) findViewById(R.id.scPlayGame);
+
+
+
+        // *** Click listener para pasar de actividades 
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PreferencesActivity.this, ProfileYouActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PreferencesActivity.this, ChataAndNotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        scPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PreferencesActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
